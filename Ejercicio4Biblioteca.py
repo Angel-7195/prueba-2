be9371831aa954eeed400a808342eded634ccd14
Angel_7195 <angelgutierrez1132998@correo.itm.edu.co>
2025-08-23 20:21:10 -0500

    correccion ejercicio4

--- Ejercicio4Biblioteca.py
+++ Ejercicio4Biblioteca.py
@@ -6,12 +6,6 @@ class Libro:
         self.disponible = True
         self.prestado_a = None
 
-    def __str__(self):
-        return f"{self.titulo} ({self.autor})"
-    
-    def __repr__(self):
-        return self.__str__()
-
     def prestar(self, estudiante):
         if self.disponible:
             self.disponible = False
@@ -19,7 +13,7 @@ class Libro:
             estudiante.agregar_libro(self)
             return f"Libro '{self.titulo}' prestado a {estudiante.nombre}"
         else:
-            return "ya esta prestado"
+            return f"El libro '{self.titulo}' ya esta prestado"
         
     def devolver(self):
         if not self.disponible:
@@ -28,7 +22,7 @@ class Libro:
             self.prestado_a = None
             return f"Libro '{self.titulo}' devuelto con exito"
         else:
-            return "ya esta devuelto"
+            return f"El libro '{self.titulo}' ya estaba disponible"
     
 class Estudiante:
     def __init__(self, nombre, ID):
@@ -43,17 +37,52 @@ class Estudiante:
         if libro in self.libros_prestados:
             self.libros_prestados.remove(libro)
         else:
-            return "El estudiante '{self.nombre}' no tiene este libro"
+            return f"El estudiante '{self.nombre}' no tiene este libro"
 
 class Biblioteca:
     def __init__(self):
         self.libros = []
         self.estudiantes = []
 
-libro1 = Libro("Cien años de soledad", "Gabriel Garcia M", "12345")
+    def agregar_libro(self, libro):
+        self.libros.append(libro)
+
+    def agregar_estudiante(self, estudiante):
+        self.estudiantes.append(estudiante)
+
+    def mostrar_inventario(self):
+        if not self.libros:
+            print("no hay libros en la biblioteca")
+        else:
+            print("\n inventario de la biblioteca:")
+            for libro in self.libros:
+                if libro.disponible:
+                    estado = "Disponible"
+                else:
+                    estado = f"Prestado a {libro.prestado_a.nombre}"
+                print(f"- Titulo: {libro.titulo}, Autor: ({libro.autor}), Estado: {estado}")
+
+biblioteca = Biblioteca()
+
+libro1 = Libro("Cien años de soledad", "Gabriel García Márquez", "12345")
+libro2 = Libro("El Principito", "Antoine de Saint-Exupéry", "67890")
+
 estudiante1 = Estudiante("Ana", 1)
+estudiante2 = Estudiante("Andres", 2)
+
+biblioteca.agregar_libro(libro1)
+biblioteca.agregar_libro(libro2)
+biblioteca.agregar_estudiante(estudiante1)
+biblioteca.mostrar_inventario()
 
 print(libro1.prestar(estudiante1))
-print(estudiante1.libros_prestados)
+biblioteca.mostrar_inventario()
+
+print(libro2.prestar(estudiante2))
+biblioteca.mostrar_inventario()
+
 print(libro1.devolver())
-print(estudiante1.libros_prestados)
+biblioteca.mostrar_inventario()
+
+print(libro2.devolver())
+biblioteca.mostrar_inventario()
